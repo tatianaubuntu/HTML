2d6338049d59ad477309a7de687ce12b6ad10b8f
Tatiana Chepurnaya <tanyusha_tokor@mail.ru>
2024-04-18 14:48:51 +0500

    correct code html, add code main

---
+++ main.py
@@ -0,0 +1,35 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+
+hostName = "localhost"
+serverPort = 8080
+
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def __get_index(self):
+        with open('index.html', 'r') as file:
+            response = file.read()
+        return response
+
+    def do_GET(self):
+        query_components = parse_qs(urlparse(self.path).query)
+        page_address = query_components.get('page')
+        page_content = self.__get_index()
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(page_content, "utf-8"))
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
